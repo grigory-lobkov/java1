7e0c1758cbd8d2cfc413a644c51fe36967c28ce5
grigory-lobkov <gg81@yandex.ru>
2019-12-10 06:45:37 +0500

    setStars

--- src/Lesson10.java
+++ src/Lesson10.java
@@ -1,4 +1,6 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class Lesson10 {
 
@@ -36,6 +38,35 @@ invertWords("Буря мглою небо кроет") должен вернут
             if (i>0) result.append('.');
         }
         return result.toString();
+    }
+
+/*
+Реализовать метод с сигнатурой public String setStars(String filename)
+который читает файл filename и меняет в нем каждый 10-йбайт на символ *,
+при этом конкатенируя оригинальный символ в строку ответа.
+
+В случае ошибки выбросить исключение IOException со строкой сообщения:равной имени класса оригинального сообщения
+
+Например,при содержимом файла:
+
+0123456789012345678A012345678B01
+
+новое содержимое должно быть
+
+012345678*012345678*012345678*01
+
+и метод должен вернуть "9AB"
+*/
+
+    String setStars(String filename) {
+        try(RandomAccessFile raf = new RandomAccessFile(filename,"rw")) {
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 
     public static void main(String[] args) {
