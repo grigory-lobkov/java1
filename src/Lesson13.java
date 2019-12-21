0f215ed04f06b2c3d6a6b8129a617df3f4e4e8dc
Grigory Lobkov <gg81@ya.ru>
2019-12-22 00:19:42 +0500

    array2queue

--- src/Lesson13.java
+++ src/Lesson13.java
@@ -1,4 +1,5 @@
 import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 
 public class Lesson13 {
     void dequeueTest() {
@@ -13,7 +14,30 @@ public class Lesson13 {
         System.out.println(deque);
     }
 
+    void pqTest() {
+        PriorityQueue pQueue = new PriorityQueue<>();
+        pQueue.offer(10);
+        pQueue.offer(1);
+        pQueue.offer(9);
+        pQueue.offer(3);
+        System.out.println(pQueue);
+    }
+
+//Создайте метод с сигнатурой ArrayDeque<Integer> array2queue(int[] a),
+// который копирует содержимое массива в очередь
+
+    ArrayDeque<Integer> array2queue(int[] a) {
+        ArrayDeque<Integer> result = new ArrayDeque<Integer>();
+        if (a == null) return result;
+        for (int e : a) {
+            result.offer(e);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        new Lesson13().dequeueTest();
+        //new Lesson13().dequeueTest();
+        //new Lesson13().pqTest();
+        System.out.println(new Lesson13().array2queue(new int[]{4, 2, 1}));
     }
 }
