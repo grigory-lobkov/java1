3b95c1560c7690417a7d4a73e2b8e80c02da41e4
Grigory Lobkov <gg81@ya.ru>
2019-12-21 13:49:42 +0500

    existInAll

--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -63,13 +63,75 @@ public class ProductAnalytics {
 
     //товары из products, которые имеются во всех магазинах
     public Set<Product> existInAll() {
+        if (allProducts == null || allProducts.size() == 0) return new HashSet<>();
         if (shops == null || shops.size() == 0) return new HashSet<>();
-        Iterator it = shops.iterator();
-        //Set<Product> products = new HashSet<Product>(((Shop)it.next()).getProducts());
+
+        //Set<Product> products = new HashSet<Product>(((Shop)it.next()).getProducts()); //надо обязательно перечесть с "товары из products", а так могли бы сэономить
         Set<Product> products = new HashSet<Product>(allProducts);
+
+        Iterator it = shops.iterator();
         while (it.hasNext()) {
             products.retainAll(new HashSet<Product>(((Shop) it.next()).getProducts()));
         }
         return products;
     }
+
+    //товары из products, которые имеются хотя бы в одном магазине
+    public Set<Product> existAtListInOne() {
+        if (shops == null || shops.size() == 0) return new HashSet<>();
+
+        Iterator it = shops.iterator();
+        //Set<Product> products = new HashSet<Product>(allProducts);
+        Set<Product> products = new HashSet<Product>(((Shop) it.next()).getProducts());
+        while (it.hasNext()) {
+            products.addAll(new HashSet<Product>(((Shop) it.next()).getProducts()));
+        }
+        products.retainAll(allProducts);
+        return products;
+    }
+
+    //товары из products, которых нет ни в одном магазине
+    public Set<Product> notExistInShops() {
+        if (allProducts == null || allProducts.size() == 0) return new HashSet<>();
+
+        Set<Product> products = new HashSet<Product>(allProducts);
+        if (shops == null || shops.size() == 0) return products;
+
+        Iterator it = shops.iterator();
+        while (it.hasNext()) {
+            products.retainAll(new HashSet<Product>(((Shop) it.next()).getProducts()));
+        }
+        return products;
+    }
+
+    //товары из products, которые есть только в одном магазине
+    // это всё дикость, и не оптимально по быстродействию, но думать как сэкономить - некогда :(
+    // всё зависит от вхдных данных, какие будут соотношения: каких данных больше, продуктов или магазинов, allProducts или продуктов в каждом магазине
+    public Set<Product> existOnlyInOne() {
+        Set<Product> products = new HashSet<Product>();
+        if (allProducts == null || allProducts.size() == 0) return products;
+        if (shops == null || shops.size() == 0) return products;
+        for (int i = 0; i < shops.size(); i++) {
+            Set<Product> productsBefore = new HashSet<Product>();
+            Set<Product> products1 = new HashSet<Product>();
+            Set<Product> productsNow;
+            Iterator it = shops.iterator();
+            for (int k = 0; k < shops.size(); k++) {
+                productsNow = new HashSet<Product>(((Shop) it.next()).getProducts());
+                productsNow.retainAll(allProducts);
+                if (k < i) {
+                    productsBefore.addAll(productsNow);
+                } else if (k == i) {
+                    products1 = productsNow;
+                    products1.retainAll(productsBefore);
+                } else {
+                    products1.retainAll(productsNow);
+                }
+            }
+            products.addAll(products1);
+        }
+
+        return products;
+    }
+
 }
