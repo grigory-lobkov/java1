dea924d98e6fb73ace4bfd252d3bbd3f38cb6646
Grigory Lobkov <gg81@ya.ru>
2020-01-08 12:47:15 +0500

    checkAndAdd

--- src/Lesson14.java
+++ src/Lesson14.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Lesson14 {
 
@@ -26,5 +27,41 @@ public class Lesson14 {
             if (oldVal == null) result++;
         }
         return result;
+    }
+
+    //Реализуйте метод с сигнатурой void checkAndAdd(TreeMap<Integer, String> map, Integer key, String value),
+    // который добавляет пару key-value в map при выполнении следующих условий:
+    //
+    //значение с таким key должно отсутствовать
+    //значение key долно быть больше головы TreeMap
+    //значение key долно быть меньше хвоста TreeMap
+    static void checkAndAdd(TreeMap<Integer, String> map, Integer key, String value) {
+        //System.out.println("\nkey=" + key);
+        if (map.containsKey(key) || map == null || map.size() < 2) return;
+        Integer first = map.firstKey();
+        //System.out.println("first=" + first);
+        if (key < first) return;
+        Integer last = map.lastKey();
+        //System.out.println("last=" + last);
+        if (key > last) return;
+        map.put(key, value);
+        //System.out.println("putted!");
+    }
+
+    public static void main(String[] args) {
+        TreeMap<Integer, String> map = new TreeMap<>();
+        map.put(1, "Один");
+        map.put(9, "Девять");
+        checkAndAdd(map, 0, "Zero");
+        checkAndAdd(map, 3, "Три");
+        checkAndAdd(map, 2, "Два");
+        checkAndAdd(map, 3, "Three");
+        checkAndAdd(map, 10, "Ten");
+        System.out.println(map);
+
+        map = new TreeMap<>();
+        checkAndAdd(map, 0, "Zero");
+        checkAndAdd(map, 0, "Zero");
+        System.out.println(map);
     }
 }
