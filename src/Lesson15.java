4f75af11a19c3979e671e77475f1bebada184fbe
grigory-lobkov <gg81@yandex.ru>
2020-01-10 08:32:34 +0500

    createDate

---
+++ src/Lesson15.java
@@ -0,0 +1,16 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class Lesson15 {
+
+    //Напишите метод с сигнатурой Date createDate(), который возвращает дату - 28 февраля 1986, 0 часов, 0 минут, 0 секунд, 0 миллисекунд
+    static Date createDate() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(1986,1,28,0,0,0);
+        return cal.getTime();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(createDate());
+    }
+}
