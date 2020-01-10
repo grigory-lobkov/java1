8ec4e98f30a8c0dc35c2ea87c696410a0ab7e479
grigory-lobkov <gg81@yandex.ru>
2020-01-10 10:19:35 +0500

    createInstant

--- src/Lesson15.java
+++ src/Lesson15.java
@@ -1,5 +1,7 @@
+import java.time.*;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class Lesson15 {
 
@@ -12,6 +14,26 @@ public class Lesson15 {
     }
 
     public static void main(String[] args) {
-        System.out.println(createDate());
+        /*System.out.println(createDate());
+        LocalDateTime ldt2= LocalDateTime.of(2019, 05, 05, 22, 24);
+        System.out.println(ldt2);
+        ZoneId zid1 = ZoneId.of("Europe/Moscow");
+        System.out.println(zid1.getRules().getOffset(Instant.now()));
+        System.out.println(createInstant());*/
+        System.out.println(parseZDT("01.01.2020 16:27:14.444 +0300 Moscow Standard Time"));
+    }
+
+    //Создайте метод с сигнатурой Instant createInstant(), который возвращает Instant,
+    // соответствующий 1 января 2020 года, 15 часов и одна наносекунда по Московскому времени
+    static Instant createInstant() {
+        java.time.LocalDateTime ldt1 = java.time.LocalDateTime.of(2020,1,1,15,0,0,1);//.plusHours(3);
+        //соответствующий 1 января 2020 года, 15 часов и одна наносекунда по Московскому времени
+        return ldt1.toInstant(java.time.ZoneOffset.of("+03:00:00"));
+    }
+
+    //Напишите метод, с сигнатурой ZonedDateTime parseZDT(String str),
+    // который возвращает ZonedDateTime, парся строку вида "01.01.2020 16:27:14.444 +0300 Moscow Standard Time"
+    static ZonedDateTime parseZDT(String str) {
+
     }
 }
