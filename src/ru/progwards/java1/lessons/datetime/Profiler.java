0c41984b8c216104d7c17f51a2ad2ba5da07bf73
grigory-lobkov <gg81@yandex.ru>
2020-01-12 14:33:14 +0500

    Profiler

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -2,39 +2,44 @@ package ru.progwards.java1.lessons.datetime;
 
 import java.lang.reflect.Array;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 //Реализовать класс для ручной профилировки производительности программного кода
 
-class StatisticInfo {
+class StatisticInfo implements Comparable {
     public String sectionName; // имя секции
-    public int fullTime; // полное время выполнения секции в миллисекундах
-    public int selfTime; // чистое время выполнения секции в миллисекундах
-    public int count; // количество вызовов
+    public int fullTime = 0; // полное время выполнения секции в миллисекундах
+    public int selfTime = 0; // чистое время выполнения секции в миллисекундах
+    public int count = 0; // количество вызовов
 
+    private boolean isInside = true; // выполняется ли секция
     private long startMs = System.currentTimeMillis(); // время начала
+    private ArrayList<StatisticInfo> runInside = new ArrayList<StatisticInfo>();
+
+    @Override
+    public int compareTo(Object o) {
+        return this.sectionName.compareTo(((StatisticInfo)o).sectionName);
+    }
 }
 
 public class Profiler {
 
-    private static ArrayList<StatisticInfo> sections = new ArrayList<StatisticInfo>();
+    //private static ArrayList<StatisticInfo> sections = new ArrayList<StatisticInfo>();
+    static TreeMap<String, StatisticInfo> sections = new TreeMap<String, StatisticInfo>();
 
     // войти в профилировочную секцию
-    public static enterSection(String name) {
+    public static void enterSection(String name) {
 
     }
 
     // выйти из профилировочной секции
-    public static exitSection(String name) {
+    public static void exitSection(String name) {
 
     }
 
     // получить профилировочную статистику, отсортировать по наименованию секции
     public static List<StatisticInfo> getStatisticInfo() {
-        Collections.sort((List)sections);
-        return sections;
+        return new ArrayList<StatisticInfo>(sections);
     }
 
 }
