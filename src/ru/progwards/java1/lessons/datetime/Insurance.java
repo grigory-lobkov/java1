0369b19eb9cad4f140aeb85569acb0152bbc5a53
grigory-lobkov <gg81@yandex.ru>
2020-01-12 09:23:14 +0500

    Insurance

--- src/ru/progwards/java1/lessons/datetime/Insurance.java
+++ src/ru/progwards/java1/lessons/datetime/Insurance.java
@@ -1,6 +1,7 @@
 package ru.progwards.java1.lessons.datetime;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -42,19 +43,32 @@ public class Insurance {
     }
 
     // установить продолжительность действия страховки
-    public setDuration(Duration duration) {
-
+    public void setDuration(Duration duration) {
+        valid = duration;
     }
 
     // установить продолжительность действия страховки, задав дату-время окончания
-    public setDuration(ZonedDateTime stop) {
-
+    public void setDuration(ZonedDateTime stop) {
+        valid = Duration.between(start, stop);
     }
 
     // установить продолжительность действия страховки, задав целыми числами количество месяцев, дней и часов
-    public setDuration(int mons, int days, int hrs) {
-
+    public void setDuration(int mons, int days, int hrs) {
+        ZonedDateTime ldt = start == null ? ZonedDateTime.now() : start;
+        ldt = ldt.plusMonths(mons).plusDays(days).plusHours(hrs);
+        valid = Duration.between(start, ldt);
     }
 
+    // проверить валидна ли страховка на указанную дату-время
+    public boolean checkValid(ZonedDateTime dateTime) {
+        ZonedDateTime end = start.plusHours(valid.toHours());
+        return dateTime.isAfter(start) && dateTime.isBefore(end);
+    }
+
+    // вернуть строку формата "Insurance "+start+" "+duration
+    @Override
+    public String toString() {
+        return "Insurance " + start + " " + valid;
+    }
 
 }
