91a850323145a8a44674e5724c5ac4a10b9da17f
grigory-lobkov <gg81@yandex.ru>
2020-01-30 10:00:10 +0500

    OrderProcessor

--- src/ru/progwards/java1/lessons/files/OrderProcessor.java
+++ src/ru/progwards/java1/lessons/files/OrderProcessor.java
@@ -1,10 +1,14 @@
 package ru.progwards.java1.lessons.files;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Order {
     public String shopId; // идентификатор магазина
@@ -13,12 +17,33 @@ class Order {
     public LocalDateTime datetime; // дата-время заказа (из атрибутов файла - дата последнего изменения)
     public List<OrderItem> items; // список позиций в заказе, отсортированный по наименованию товара
     public double sum; // сумма стоимости всех позиций в заказе
+
+    public Order() {
+        items = new ArrayList<OrderItem>();
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "shopId='" + shopId + '\'' +
+                ", orderId='" + orderId + '\'' +
+                ", customerId='" + customerId + '\'' +
+                ", datetime=" + datetime +
+                ", items=" + items +
+                ", sum=" + sum +
+                '}';
+    }
 }
 
 class OrderItem {
     public String googsName; // наименование товара
     public int count; // количество
     public double price; // цена за единицу
+
+    @Override
+    public String toString() {
+        return "\nOrderItem{" + googsName + ", count=" + count + ", price=" + price + '}';
+    }
 }
 
 public class OrderProcessor {
@@ -44,6 +69,7 @@ ZZZZ - обязательные 4 символа customerId - идентифик
 
     Path startPath; // начальная папка для хранения файлов
     List<Order> orders; // список заказов
+    String loadedShopId; // загруженный магазин
 
     // инициализирует класс, с указанием начальной папки для хранения файлов
     public OrderProcessor(String startPath) {
@@ -58,9 +84,92 @@ ZZZZ - обязательные 4 символа customerId - идентифик
     public int loadOrders(LocalDate start, LocalDate finish, String shopId) {
         orders = new ArrayList<Order>();
         int failedFiles = 0;
+        List<Path> paths = null;
+        loadedShopId = shopId;
 
-        sortOrders(); // отсортируем orders
+        // список файлов с информацией о заказах
+        // плохо, что имена папок "не имеют значения". В имя папки или файла обязательно надо было сделать привязку к дате. Как процессинг будет работать через 10 лет!?
+        String shopFilter = shopId == null ? "???" : shopId;
+        String pattern = "glob:**/" + shopFilter + "-??????-????.csv";
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher(pattern);
+        try {
+            paths = Files.walk(startPath)
+                    .filter(p -> {
+                        if (pathMatcher.matches(p)) {
+                            if (start == null && finish == null) return true;
+                            LocalDate fm = getFileLocalDateTime(p).toLocalDate();
+                            if (fm == null) return false;
+                            return !((start != null && fm.isBefore(start)) || (finish != null && fm.isAfter(finish)));
+                        }
+                        return false;
+                    })
+                    .sorted(Comparator.comparing(this::getFileLocalDateTime))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (paths == null) return 0;
+        //System.out.println(paths);
+
+        for (Path path : paths) {
+            if (!loadOrderFromFile(path)) {
+                failedFiles++;
+                //System.out.println("Processing failed: "+path);
+            }
+        }
+        //System.out.println(orders);
+        //sortOrders(); // отсортируем orders // отсортировали файлы ранее, потому это не надо
+
         return failedFiles;
+    }
+
+    public LocalDateTime getFileLocalDateTime(Path path) {
+        try {
+            return LocalDateTime.ofInstant(Files.getLastModifiedTime(path).toInstant(), ZoneId.systemDefault());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // Загрузим заказ одного файла. Если что-то поёдет не так, вернем false, заказ игнорируем
+    public boolean loadOrderFromFile(Path path) {
+        Order order = new Order();
+        Double sum = 0d;
+        final String DELIMITER = ",";
+        Charset charset = Charset.forName("UTF-8");
+        String fileName = path.getFileName().toString();
+        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                try {
+                    String[] s = line.split(DELIMITER);
+                    if (s.length == 0) continue;
+                    if (s.length != 3) return false; // критическая ошибка
+                    OrderItem item = new OrderItem();
+                    item.googsName = s[0];
+                    item.count = Integer.parseInt(s[1].trim());
+                    item.price = Double.parseDouble(s[2].trim());
+                    sum += item.price * item.count;
+                    order.items.add(item);
+                } catch (Exception e) {
+                    System.err.println(fileName + ':' + line + ':' + e); //log
+                    return false; // критическая ошибка
+                }
+            }
+            String[] s = fileName.substring(0, fileName.lastIndexOf(".")).split("-");
+            //например S02-P01X12-0012.csv: shopId=”S02”, orderId=”P01X12”, customerId=”0012”
+            order.datetime = getFileLocalDateTime(path);
+            order.shopId = s[0];
+            order.orderId = s[1];
+            order.customerId = s[2];
+            order.sum = sum;
+            orders.add(order);
+            return true;
+        } catch (IOException e) {
+            System.err.println(fileName + ':' + e); //log
+        }
+        return false;
     }
 
     // сортировка заказов "в порядке обработки"
@@ -77,26 +186,59 @@ ZZZZ - обязательные 4 символа customerId - идентифик
     // выдать список заказов в порядке обработки (отсортированные по дате-времени), для заданного магазина.
     // Если shopId == null, то для всех
     public List<Order> process(String shopId) {
-        return null;
+        if (shopId == null || shopId.compareTo(loadedShopId) == 0) return orders;
+        List<Order> result = new ArrayList<Order>();
+        for (Order o : orders) {
+            if (o.shopId == shopId) result.add(o);
+        }
+        return result;
     }
 
     // выдать информацию по объему продаж по магазинам (отсортированную по ключам): String - shopId, double - сумма стоимости всех проданных товаров в этом магазине
     public Map<String, Double> statisticsByShop() {
-        return null;
+        Map<String, Double> result = new HashMap<String, Double>();
+        for (Order o : orders) {
+            String key = o.shopId;
+            boolean isExists = result.containsKey(key);
+            double sum = isExists ? result.get(key) : 0;
+            result.put(key, sum + o.sum);
+        }
+        return result;
     }
 
     // выдать информацию по объему продаж по товарам (отсортированную по ключам): String - goodsName, double - сумма стоимости всех проданных товаров этого наименования
     public Map<String, Double> statisticsByGoods() {
-        return null;
+        Map<String, Double> result = new HashMap<String, Double>();
+        for (Order o : orders) {
+            for (OrderItem item : o.items) {
+                String key = item.googsName;
+                boolean isExists = result.containsKey(key);
+                double sum = isExists ? result.get(key) : 0;
+                result.put(key, sum + item.count*item.price);
+            }
+        }
+        return result;
     }
 
     // выдать информацию по объему продаж по дням (отсортированную по ключам): LocalDate - конкретный день, double - сумма стоимости всех проданных товаров в этот день
     public Map<LocalDate, Double> statisticsByDay() {
-        return null;
+        Map<LocalDate, Double> result = new HashMap<LocalDate, Double>();
+        for (Order o : orders) {
+            LocalDate key = o.datetime.toLocalDate();
+            boolean isExists = result.containsKey(key);
+            double sum = isExists ? result.get(key) : 0;
+            result.put(key, sum + o.sum);
+        }
+        return result;
     }
 
     // test
     public static void main(String[] args) {
         OrderProcessor p = new OrderProcessor("C:\\Users\\Darya\\IdeaProjects\\java1\\src\\ru\\progwards\\java1\\lessons\\files\\orders");
+        p.loadOrders(LocalDate.now(), LocalDate.now(), "S02");
+        System.out.println(p.process("S02"));
+        System.out.println(p.statisticsByShop());
+        System.out.println(p.statisticsByGoods());
+        System.out.println(p.statisticsByDay());
     }
 }
